8b42246f3856ea6da36f90dc706315ab1a51b087
musjib999 <musjib999@gmail.com>
2021-08-31 16:59:59 +0100

    genesis commit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    mongoDb: {
+        host: 'localhost',
+        port: '27017',
+        username: 'musjib',
+        password: '123456',
+        database: 'pastQ'
+    }
+
+}
